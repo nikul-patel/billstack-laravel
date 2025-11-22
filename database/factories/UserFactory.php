339b4b18be772d6bcddd4d7ca86a72232a915d41
Nikul Patel <42406602+nikul-patel@users.noreply.github.com>
2025-11-22 10:35:34 -0800

    Add Laravel 12 application scaffold

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class UserFactory extends Factory
+{
+    protected $model = User::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => '$2y$12$usesomesillystringfore7hnbRJHxXVLeakoG8K30oukPsA.ztMG',
+            'remember_token' => Str::random(10),
+        ];
+    }
+
+    public function unverified(): static
+    {
+        return $this->state(fn () => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
